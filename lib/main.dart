412c522043cef2a73dd4822e732ef44395fdaedb
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2025-09-16 16:36:52 +0000

    Basic App

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Web Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Web Demo'),
+      ),
+      body: const Center(
+        child: Text('Hello, Flutter Web!'),
+      ),
+    );
+  }
+}
